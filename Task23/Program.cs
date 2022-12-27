0c21b085c69de2d936894273bb8400170c927b35
Egor <eeeee97345@gmail.com>
2022-12-27 13:51:28 +0300

    completed

---
+++ Task23/Program.cs
@@ -0,0 +1,6 @@
+﻿int num;
+Console.Write("Введите любое число: ");
+num = int.Parse(Console.ReadLine());
+for (int i = 1; i <= num; i++) Console.Write($"{Math.Pow(i, 3)}, ");
+Console.WriteLine(" ");
+return;
